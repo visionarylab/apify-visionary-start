bf89623399238e9bfb1aa6d3e32b43636e758a18
Yh010 <yash4success@gmail.com>
2024-08-13 11:26:25 +0530

    Actor that scrapes 20 posts from the Instagram website based on the hashtag

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-require('dotenv').config();
+const { ApifyClient } = require('apify-client');
 
-console.log(process.env.MY_APIFY_TOKEN);
+require('dotenv').config();
+const MY_APIFY_TOKEN = process.env.MY_APIFY_TOKEN
+/* require ApifyClient
+
+import { ApifyClient } from 'apify-client';
+ */
+
+async function main() {
+    const client = new ApifyClient({ token: MY_APIFY_TOKEN });
+
+    const actorClient = client.actor('apify/instagram-hashtag-scraper');
+
+    const input = { hashtags: ['rainbow'], resultsLimit: 20 };
+
+    const runData = await actorClient.call(input, { waitSecs: 60 });
+
+    console.log("Run data:");
+    console.log(runData);    
+}
+
+main();
